ce65abe2bd45b0f2e98eeee7dd55236cff9b8257
Giovanny Piovesan <giovanny.piovesan@nubank.com.br>
2023-05-16 17:57:18 -0300

    project scaffold

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_sample/pages/home_page.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
